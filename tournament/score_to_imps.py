ad7618e0b8e47b506cf5d50cd5b69e6b63946d11
ugobindini <ugobindini@yahoo.it>
2018-07-12 16:51:57 +0200

    score_to_imps aggiornato, può gestire valori negativi

--- tournament/score_to_imps.py
+++ tournament/score_to_imps.py
@@ -1,9 +1,12 @@
 IMP_TABLE = [0, 20, 50, 90, 130 ,170, 220, 270, 320, 370, 430, 500, 600, 750, 900, 1100, 1300, 1500, 1750, 2000, 2250, 2500, 3000, 3500, 4000, 1000000]
 
 def score_to_imps(score):
-	"Given an integer score, it is tranformed to IMPs via IMP_TABLE"
+	"Given an integer score, it is tranformed to IMPs via IMP_TABLE, mantaing the same sign of score (positive/negative)"
 	i = 0
 	while score > IMP_TABLE[i]:
 		i += 1
 	
-	return i
+	if score > 0:
+		return i
+	else
+		return -i
